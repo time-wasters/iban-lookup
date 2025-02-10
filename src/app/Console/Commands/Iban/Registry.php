b65a1cc5abbbdd6eb95737c863f10c534bef0e1d
plp-GTR <github@plpgtr.de>
2025-02-10 23:21:54 +0100

    artisan make:command Iban/Registry --test

---
+++ src/app/Console/Commands/Iban/Registry.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands\Iban;
+
+use Illuminate\Console\Command;
+
+class Registry extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:registry';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
