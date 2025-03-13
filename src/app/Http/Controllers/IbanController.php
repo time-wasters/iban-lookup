273ecfa99aba9f2167b18ebf3add5e7990f8011a
plp-GTR <github@plpgtr.de>
2025-03-13 21:37:21 +0100

    sail artisan make:model Iban -mfcrR

---
+++ src/app/Http/Controllers/IbanController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreIbanRequest;
+use App\Http\Requests\UpdateIbanRequest;
+use App\Models\Iban;
+
+class IbanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreIbanRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Iban $iban)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Iban $iban)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateIbanRequest $request, Iban $iban)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Iban $iban)
+    {
+        //
+    }
+}
